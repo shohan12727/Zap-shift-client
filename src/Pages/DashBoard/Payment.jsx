0e5af5d7ee6b297b325d9f674251d18db58c2797
shohan12727 <aishohan001@gmail.com>
2025-12-02 19:37:41 +0600

    implement parcel payment retrieval and loading state

--- src/Pages/DashBoard/Payment.jsx
+++ src/Pages/DashBoard/Payment.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import { useParams } from "react-router";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
 
 const Payment = () => {
+  const { parcelId } = useParams();
+  const axiosSecure = useAxiosSecure();
+
+  const { isLoading, data: parcel } = useQuery({
+    queryKey: ["parcels", parcelId],
+    queryFn: async () => {
+      const response = await axiosSecure.get(`/parcels/${parcelId}`);
+      return response.data;
+    },
+  });
+
+  if (isLoading) {
     return (
-        <div>
-            <h2>Please pay</h2>
-        </div>
+      <div className="h-screen flex justify-center items-center">
+        <span className="loading loading-infinity loading-xl"></span>
+      </div>
     );
+  }
+
+  return (
+    <div>
+      <h2>Please pay for : {parcel.parcelName}</h2>
+      <button className= "btn btn-primary text-black">Pay</button>
+    </div>
+  );
 };
 
 export default Payment;
