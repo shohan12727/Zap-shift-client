7cdbb05d28ba51f02dca4cfc1562b795f732c157
shohan12727 <aishohan001@gmail.com>
2025-12-04 12:01:39 +0600

    refactor useAxiosSecure to create axios instance within the hook and set up request interceptor for authorization

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,10 +1,20 @@
 import axios from "axios";
-import React from "react";
+import { useEffect } from "react";
+import useAuth from "./useAuth";
+
+const axiosSecure = axios.create({
+  baseURL: "http://localhost:3000",
+});
 
 const useAxiosSecure = () => {
-  const axiosSecure = axios.create({
-    baseURL: "http://localhost:3000",
-  });
+  const { user } = useAuth();
+  useEffect(() => {
+    // intercept request
+    axiosSecure.interceptors.request.use((config) => {
+      config.headers.Authorization = `Bearer ${user?.accessToken}`;
+      return config;
+    });
+  }, [user?.accessToken]);
 
   return axiosSecure;
 };
