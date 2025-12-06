6d6d438ab8bf89a724cba87d14194f71f2dafca7
shohan12727 <aishohan001@gmail.com>
2025-12-06 16:14:42 +0600

    refactor: enhance user role management with SweetAlert notifications and update UI actions

--- src/Pages/DashBoard/UsersManagement.jsx
+++ src/Pages/DashBoard/UsersManagement.jsx
@@ -1,16 +1,49 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../Hooks/useAxiosSecure";
+import { FaUserShield } from "react-icons/fa";
+import { FiShieldOff } from "react-icons/fi";
+import Swal from "sweetalert2";
 
 const UsersManagement = () => {
   const axiosSecure = useAxiosSecure();
-  const { data: users = [] } = useQuery({
+  const { refetch, data: users = [] } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
       const res = await axiosSecure.get(`/users`);
-      console.log(res.data);
       return res.data;
     },
   });
+
+  const handleMakeUser = (user) => {
+    const roleInfo = { role: "admin" };
+    axiosSecure.patch(`/users/${user._id}`, roleInfo).then((res) => {
+      if (res.data.modifiedCount) {
+        refetch();
+        Swal.fire({
+          title: `${user.displayName}  marked as an Admin`,
+          icon: "success",
+          draggable: true,
+        });
+      }
+    });
+  };
+
+  const removeAdmin = (user) => {
+    const roleInfo = {
+      role: "user",
+    };
+    axiosSecure.patch(`/users/${user._id}`, roleInfo).then((res) => {
+      if (res.data.modifiedCount) {
+        refetch();
+        Swal.fire({
+          title: `${user.displayName}  remove from Admin`,
+          icon: "success",
+          draggable: true,
+        });
+      }
+    });
+  };
+
   return (
     <div>
       <h2 className="text-4xl">Manage users : {users.length}</h2>
@@ -20,10 +53,11 @@ const UsersManagement = () => {
           <thead>
             <tr>
               <th>No</th>
-              <th>Name</th>
+              <th>User</th>
               <th>Email</th>
-              <th>Favorite Color</th>
-              <th></th>
+
+              <th>Admin Actions</th>
+              <th>Other Actions</th>
             </tr>
           </thead>
           <tbody>
@@ -35,10 +69,7 @@ const UsersManagement = () => {
                   <div className="flex items-center gap-3">
                     <div className="avatar">
                       <div className="mask mask-squircle h-12 w-12">
-                        <img
-                          src={user.photoURL}
-                          alt="Avatar"
-                        />
+                        <img src={user.photoURL} alt="Avatar" />
                       </div>
                     </div>
                     <div>
@@ -47,12 +78,24 @@ const UsersManagement = () => {
                     </div>
                   </div>
                 </td>
-                <td>
-                 {user.email    }
-                </td>
-                <td>Purple</td>
+                <td>{user.email}</td>
+                <td>{user.role}</td>
                 <th>
-                  <button className="btn btn-ghost btn-xs">details</button>
+                  {user.role === "admin" ? (
+                    <button
+                      className="btn bg-red-400"
+                      onClick={() => removeAdmin(user)}
+                    >
+                      <FaUserShield />
+                    </button>
+                  ) : (
+                    <button
+                      className="btn bg-green-400"
+                      onClick={() => handleMakeUser(user)}
+                    >
+                      <FiShieldOff />
+                    </button>
+                  )}
                 </th>
               </tr>
             ))}
